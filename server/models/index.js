1ef7e6b3392eb14dfd5080dc1a7d8a0abd7fde79
James Enejo <james.enejo@andela.com>
2018-09-03 18:17:03 +0100

    refactor config file for sequelize connection

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
-import dbConfig from '../config/dbConfig';
+import dbConfig from '../config/config';
 
 const basename = path.basename(module.filename);
 const config = dbConfig[process.env.NODE_ENV];
