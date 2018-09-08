34b9d8143aea8cc383bfe3225e9369d7172f7122
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-09-08 21:33:40 +0100

    feat(auth): fix password issue for new users

--- test/test.spec.js
+++ test/test.spec.js
@@ -338,3 +338,39 @@ describe('USER SIGN IN TEST', () => {
       });
   });
 });
+
+describe('SOCIAL MEDIA AUTHENTICATION', () => {
+  before(() => {
+    nock('https://accounts.google.com')
+      .filteringPath(() => '/')
+      .get('/')
+      .reply(200, 'Google auth request sent.');
+
+    nock('https://www.facebook.com/')
+      .filteringPath(() => '/')
+      .get('/')
+      .reply(200, 'Facebook auth request sent.');
+  });
+
+  // This test proves that the google authentication route is called
+  it('Authenticate with google', ((done) => {
+    chai
+      .request(app)
+      .get('/api/v1/users/auth/google')
+      .end((err, res) => {
+        expect(res.text).to.be.equal('Google auth request sent.');
+        done();
+      });
+  }));
+
+  // This test proves that the facebook authentication route is called
+  it('Authenticate with facebook', ((done) => {
+    chai
+      .request(app)
+      .get('/api/v1/users/auth/facebook')
+      .end((err, res) => {
+        expect(res.text).to.be.equal('Facebook auth request sent.');
+        done();
+      });
+  }));
+});
