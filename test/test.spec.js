c2a231930b91fafa12ff46ed249fe317665fc4be
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-09-11 11:34:05 +0100

    feat(authentication): modularize social media login test

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../server/app';
+import './socialLogin.spec';
 
 chai.use(chaiHttp);
 const { should, expect } = chai;
@@ -337,40 +338,4 @@ describe('USER SIGN IN TEST', () => {
         done();
       });
   });
-});
-
-describe('SOCIAL MEDIA AUTHENTICATION', () => {
-  before(() => {
-    nock('https://accounts.google.com')
-      .filteringPath(() => '/')
-      .get('/')
-      .reply(200, 'Google auth request sent.');
-
-    nock('https://www.facebook.com/')
-      .filteringPath(() => '/')
-      .get('/')
-      .reply(200, 'Facebook auth request sent.');
-  });
-
-  // This test proves that the google authentication route is called
-  it('Authenticate with google', ((done) => {
-    chai
-      .request(app)
-      .get('/api/v1/users/auth/google')
-      .end((err, res) => {
-        expect(res.text).to.be.equal('Google auth request sent.');
-        done();
-      });
-  }));
-
-  // This test proves that the facebook authentication route is called
-  it('Authenticate with facebook', ((done) => {
-    chai
-      .request(app)
-      .get('/api/v1/users/auth/facebook')
-      .end((err, res) => {
-        expect(res.text).to.be.equal('Facebook auth request sent.');
-        done();
-      });
-  }));
 });
