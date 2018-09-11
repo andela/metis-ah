a64ff358608555d590bf285bb4de41d345aeba25
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-09-11 14:09:18 +0100

    chore(auth): fix styling issues

--- server/config/passportSetup.js
+++ server/config/passportSetup.js
@@ -10,41 +10,27 @@ const { Users } = model;
 
 dotenv.config();
 
-passport.serializeUser(
-  (user, done) => {
-    done(null, user.id);
-  }
-);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
-passport.deserializeUser(
-  (id, done) => {
-    Users.findById(id).then(
-      (user) => {
-        done(null, user);
-      }
-    );
-  }
-);
+passport.deserializeUser((id, done) => {
+  Users.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
-passport.use(
-  new GoogleStrategy(
-    {
-      callbackURL: 'http://localhost:8000/api/users/auth/google/redirect',
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }, helpers.googlesCallback
-  )
-);
+passport.use(new GoogleStrategy({
+  callbackURL: 'http://localhost:8000/api/users/auth/google/redirect',
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET
+}, helpers.googlesCallback));
 
-passport.use(
-  new FaceBookStrategy(
-    {
-      clientID: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      callbackURL: 'http://localhost:8000/api/users/auth/facebook/redirect',
-      profileFields: ['email']
-    }, helpers.facebookCallback
-  )
-);
+passport.use(new FaceBookStrategy({
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/api/users/auth/facebook/redirect',
+  profileFields: ['email']
+}, helpers.facebookCallback));
 
 export default passport;
