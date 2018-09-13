3c66b45e525b9b6fa739804f56130f8e6850ead6
DanielAdek <maildaniel.me1@gmail.com>
2018-09-13 11:56:50 +0100

    feat: (list users) Retrieve all Users
    -create get users functionality
    -test get users functionality
    -document functionality in swagger
    
    [(Delivers: #159987413)]

--- server/seeders/20180910100446-verifieduser.js
+++ server/seeders/20180910100446-verifieduser.js
@@ -8,6 +8,14 @@ module.exports = {
     isVerified: true,
     createdAt: '2018-09-08',
     updatedAt: '2018-09-08'
+  },
+  {
+    username: 'postman',
+    email: 'postman@gmail.com',
+    password: '$2b$08$Oei0G0AOs5ZflnVQorfW3evz2a.L3IHKWQVaNd0OP0mTUPS2NEfPq',
+    isVerified: false,
+    createdAt: '2018-09-08',
+    updatedAt: '2018-09-08'
   }]),
   down: queryInterface => queryInterface.bulkDelete('Users')
 };
