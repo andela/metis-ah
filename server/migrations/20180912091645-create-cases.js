0006fcd58f4b7320c74744d0e4d6ae75e29b5d13
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-09-13 11:30:22 +0100

    feat(articles): get all reported cases> - add data seeders> - get all reported cased> - document this feature[Finishes #159987418]

--- server/migrations/20180912091645-create-cases.js
+++ server/migrations/20180912091645-create-cases.js
@@ -1,56 +1,52 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Cases', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        onDelete: 'SET NULL',
-        references: {
-          model: 'Users',
-          key: 'id',
-          as: 'userId'
-        }
-      },
-      articleId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        onDelete: 'SET NULL',
-        references: {
-          model: 'Articles',
-          key: 'id',
-          as: 'articleId'
-        }
-      },
-      violation: {
-        allowNull: false,
-        type: Sequelize.ENUM('Discrimination', 'Plagiarism', 'Sexual Content', 'Offensive Language'),
-      },
-      description: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      resolved: {
-        allowNull: false,
-        type: Sequelize.BOOLEAN,
-        defaultValue: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Cases', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    userId: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      onDelete: 'SET NULL',
+      references: {
+        model: 'Users',
+        key: 'id',
+        as: 'userId'
       }
-    });
-  },
-  down: (queryInterface) => {
-    return queryInterface.dropTable('Cases');
-  }
+    },
+    articleId: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      onDelete: 'SET NULL',
+      references: {
+        model: 'Articles',
+        key: 'id',
+        as: 'articleId'
+      }
+    },
+    violation: {
+      allowNull: false,
+      type: Sequelize.ENUM('Discrimination', 'Plagiarism', 'Sexual Content', 'Offensive Language'),
+    },
+    description: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    resolved: {
+      allowNull: false,
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Cases')
 };
